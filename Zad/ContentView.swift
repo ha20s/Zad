a91e41a415ca4d131fe7d42a766066a3f2266fc8
Hasna <hassna.ahmad98@gmail.com>
2023-12-28 09:50:47 +0300

    Initial Commit

---
+++ Zad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zad
+//
+//  Created by Hasna Ahmad on 15/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zad/ZadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZadApp.swift
+//  Zad
+//
+//  Created by Hasna Ahmad on 15/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ZadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
